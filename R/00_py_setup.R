a7d285bfd01700ac0f7ed3faea3836e5af8d7fe1
Tyson Barrett <t.barrett88@gmail.com>
2024-03-03 23:46:47 -0700

    add force arg to py_setup

--- R/00_py_setup.R
+++ R/00_py_setup.R
@@ -3,14 +3,15 @@
 #' @description The function sets up a miniconda environment at `path_to_env`
 #'
 #' @param path_to_env The path to the miniconda environment
+#' @param force Whether to force an a new install of minoconda
 #'
 #' @import reticulate
 #'
 #' @export
-py_setup <- function(path_to_env){
+py_setup <- function(path_to_env, force = FALSE){
   # Set Up Python
   path = file.path(path_to_env, "r-py-automate")
-  reticulate::install_miniconda(path)
+  reticulate::install_miniconda(path, force = force)
   reticulate::use_miniconda(path)
   packages = c("ffmpeg-python", "numpy", "scipy", "setuptools-rust", "pydub", "llvmlite", "librosa", "numba",
                "Cmake", "wheel", "setuptools-rust", "pytorch", "torchvision")
