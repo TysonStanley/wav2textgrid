bccc1a7ba4d49df6cb2e987f03d76633d31dcc6d
Tyson Barrett <t.barrett88@gmail.com>
2024-03-04 19:42:25 -0700

    trying to fix that Error #15

--- R/00_py_setup.R
+++ R/00_py_setup.R
@@ -13,10 +13,11 @@ py_setup <- function(path_to_env, force = FALSE){
   path = file.path(path_to_env, "r-py-automate")
   reticulate::install_miniconda(path, force = force)
   reticulate::use_miniconda(path)
+  reticulate::conda_install(envname = path, packages = "nomkl")
   packages = c("ffmpeg-python", "numpy", "scipy", "setuptools-rust", "pydub", "llvmlite", "librosa", "numba",
                "Cmake", "wheel", "setuptools-rust", "pytorch", "torchvision")
   reticulate::conda_install(envname = path, packages = packages)
   reticulate::py_install("openai-whisper", pip = TRUE, pip_options = "-U")
-  reticulate::py_install("pyannote.audio", pip = TRUE, pip_options = "-U")
-  reticulate::py_install("light-the-torch", pip = TRUE, pip_options = "-U")
+  #reticulate::py_install("pyannote.audio", pip = TRUE, pip_options = "-U")
+  #reticulate::py_install("light-the-torch", pip = TRUE, pip_options = "-U")
 }
