ed2b11133f30dc21598716f2e75fdd4761d58098
Tyson Barrett <t.barrett88@gmail.com>
2024-05-30 12:59:03 -0600

    fix noise_reduction

--- R/01_split_channels.R
+++ R/01_split_channels.R
@@ -23,50 +23,21 @@ split_channels <- function(wav_file, noise_reduction = FALSE, threshold = 200, p
   ch1 <- stringr::str_replace(wav_file, "\\.wav$", "_ch1.wav")
   ch2 <- stringr::str_replace(wav_file, "\\.wav$", "_ch2.wav")
 
+  # split into left and right
+  left <- tuneR::channel(wav, which = "left")
+  right <- tuneR::channel(wav, which = "right")
+
+  # save channeled wav files
+  tuneR::writeWave(left, ch1)
+  tuneR::writeWave(right, ch2)
+
+  if (plot) tuneR::plot(left); tuneR::plot(right)
+
+
   # noise reduction
   if (noise_reduction){
-    script <- glue::glue({"
-# Made by Lotte Eijk - 17-11-22
-# Reduce noise using the standard Praat settings and save denoised file to specified folder
-
-form Reduce noise
-    sentence inputDir {folder}
-    positive Channel: 2
-    sentence outputDir {folder}
-    sentence inputFile {wav_file}
-endform"})
-    script <- glue::glue(script, '\n\n', "Create Strings as file list... file-list 'inputFile$'")
-    script <- glue::glue(script, '\n\n', "numberOfFiles = Get number of strings")
-    script <- glue::glue(script, '\n\n', "for ifile to numberOfFiles")
-    script <- glue::glue(script, '\n', "    select Strings list")
-    script <- glue::glue(script, '\n', "    fileName$ = Get string... ifile")
-    script <- glue::glue(script, '\n', "    Read from file... 'inputDir$'/'fileName$'")
-    script <- glue::glue(script, '\n', '    Reduce noise: 0, 0, 0.025, 80, 10000, 40, -20, "spectral-subtraction"')
-    script <- glue::glue(script, '\n', "    lengthFN = length (fileName$)")
-    script <- glue::glue(script, '\n', "    newfilename$ = fileName$")
-    script <- glue::glue(script, '\n', "    Write to WAV file... 'outputDir$'/'newfilename$'")
-    script <- glue::glue(script, '\n', "endfor")
-    script <- glue::glue(script, '\n\n', "select all")
-    script <- glue::glue(script, '\n', "Remove")
-    script_file <- paste0(folder, "temp_noise.praat")
-    writeLines(script, con = script_file)
-
-    speakr::praat_run(script_file, folder, '""', 1)
-
-    # script <- glue::glue(script, '\n\n', "printline 'directory$'")
-    # script <- glue::glue(script, '\n', "printline 'word$'")
-    # script <- glue::glue(script, '\n', "printline 'number_of_files'")
-
-  } else {
-    # split into left and right
-    left <- tuneR::channel(wav, which = "left")
-    right <- tuneR::channel(wav, which = "right")
-
-    # save channeled wav files
-    tuneR::writeWave(left, ch1)
-    tuneR::writeWave(right, ch2)
-
-    if (plot) tuneR::plot(left); tuneR::plot(right)
+    noise_reduce(fs::path_dir(ch1), ch1)
+    noise_reduce(fs::path_dir(ch2), ch2)
 
   }
 
@@ -77,3 +48,39 @@ endform"})
   return(c(ch1, ch2))
 }
 
+
+
+# create and run script for noise_reduction
+noise_reduce <- function(folder, channel){
+  script <- glue::glue({"
+# Made by Lotte Eijk - 17-11-22
+# Reduce noise using the standard Praat settings and save denoised file to specified folder
+
+form Reduce noise
+    sentence inputDir {folder}
+    positive Channel: 2
+    sentence outputDir {folder}
+    sentence inputFile {channel}
+endform"})
+  script <- glue::glue(script, '\n\n', "Create Strings as file list... file-list 'inputFile$'")
+  script <- glue::glue(script, '\n\n', "numberOfFiles = Get number of strings")
+  script <- glue::glue(script, '\n\n', "for ifile to numberOfFiles")
+  script <- glue::glue(script, '\n', "    select Strings list")
+  script <- glue::glue(script, '\n', "    fileName$ = Get string... ifile")
+  script <- glue::glue(script, '\n', "    Read from file... 'inputDir$'/'fileName$'")
+  script <- glue::glue(script, '\n', '    Reduce noise: 0, 0, 0.025, 80, 10000, 40, -20, "spectral-subtraction"')
+  script <- glue::glue(script, '\n', "    lengthFN = length (fileName$)")
+  script <- glue::glue(script, '\n', "    newfilename$ = fileName$")
+  script <- glue::glue(script, '\n', "    Write to WAV file... 'outputDir$'/'newfilename$'")
+  script <- glue::glue(script, '\n', "endfor")
+  script <- glue::glue(script, '\n\n', "select all")
+  script <- glue::glue(script, '\n', "Remove")
+  script_file <- paste0(folder, "temp_noise.praat")
+  writeLines(script, con = script_file)
+
+  speakr::praat_run(script_file, folder, '""', 1)
+}
+
+# script <- glue::glue(script, '\n\n', "printline 'directory$'")
+# script <- glue::glue(script, '\n', "printline 'word$'")
+# script <- glue::glue(script, '\n', "printline 'number_of_files'")
