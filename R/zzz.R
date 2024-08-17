630ff2a65a2479f6428d40ef9ff708d9df2fae65
Tyson Barrett <t.barrett88@gmail.com>
2024-08-16 23:23:56 -0600

    automate praat path on attach and on load

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,39 @@
 .onLoad <- function(libname = find.package("wav2textgrid"),
                     pkgname = "wav2textgrid") {
+
+  # global vars
   if (getRversion() >= "2.15.1") {
     utils::globalVariables(c("osVersion", "end", "xmin", "folder", "xmax", "annotation_num", ".", "1", "2", "channel", "dist", "dom", "end.x", "i.end", "i.id", "i.text", "id", "min_dist", "start", "start.x", "text"))
   }
+
+  # find praat
+  sys = Sys.info()[['sysname']]
+  if (sys == "Darwin") path = "/Applications/Praat.app/Contents/MacOS/Praat"
+  if (sys == "Linux") path = "/usr/bin/praat"
+  if (sys == "Windows") path = "C:/Program Files/Praat.exe"
+
+  if (file.exists(path)){
+    options(wav2textgrid.praat.path = path)
+  }
+
+  # finish it up
   invisible()
 }
+
+.onAttach <- function(libname = find.package("wav2textgrid"),
+                      pkgname = "wav2textgrid") {
+
+  path = getOption("wav2textgrid.praat.path")
+
+  if (file.exists(path)){
+    packageStartupMessage(paste("Praat found at", path))
+  } else
+    packageStartupMessage(paste("Did not find Praat at default location (", path, ").\nPlease run `set_praat_path()` with the path to your Praat application."))
+
+  # finish it up
+  invisible()
+}
+
+
+
+
