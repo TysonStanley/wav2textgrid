c6a1bf17257e79f0d69eb1db3fc71927dd0ecdbf
Tyson Barrett <t.barrett88@gmail.com>
2024-08-16 23:24:35 -0600

    add py_setup

--- R/00_py_setup.R
+++ R/00_py_setup.R
@@ -2,19 +2,25 @@
 #'
 #' @description The function sets up a miniconda environment at `path_to_env`
 #'
-#' @param path_to_env The path to the miniconda environment
-#' @param force Whether to force an a new install of minoconda
+#' @param path The path to the miniconda environment
+#' @param install Whether to install conda at the specified path (default = TRUE)
 #'
 #' @import reticulate
+#' @importFrom cli cli_process_start
+#' @importFrom cli cli_process_done
 #'
 #' @export
-py_setup <- function(path_to_env, force = FALSE){
-  # reticulate::miniconda_uninstall()
-  # reticulate::install_miniconda()
-  reticulate::use_miniconda("/Users/tysonbarrett/Library/r-miniconda-arm64")
-  # packages = c("ffmpeg-python", "numpy", "scipy", "setuptools-rust", "pydub", "llvmlite", "librosa", "numba",
-  #              "Cmake", "wheel", "setuptools-rust", "pytorch", "torchvision")
-  # reticulate::conda_install(envname = "~/Desktop/Automation/r-py-automate", packages = packages)
-  # reticulate::py_install("openai-whisper", pip = TRUE, pip_options = "-U")
-  # reticulate::py_install("light-the-torch", pip = TRUE, pip_options = "-U")
+py_setup <- function(path, install = TRUE){
+  cli::cli_process_start(msg = "Begin python environment set up")
+  if (install) {
+    reticulate::conda_create(envname = path, python_version = "3.11")
+  }
+
+  reticulate::use_condaenv(path, required = TRUE)
+  packages = c("ffmpeg-python", "numpy", "scipy", "setuptools-rust", "pydub", "llvmlite", "librosa", "numba",
+               "Cmake", "wheel", "setuptools-rust", "pytorch", "torchvision")
+  reticulate::conda_install(envname = path, packages = packages)
+  reticulate::py_install("openai-whisper", pip = TRUE, pip_options = "-U")
+  reticulate::py_install("light-the-torch", pip = TRUE, pip_options = "-U")
+  cli::cli_process_done(msg_done = "Completed python environment set up")
 }
